a1ebdd03bb0ded591b92ea7e9d76767de9d0d900
Antonio Perchinumio <antonio_perchinumio@otb.net>
2025-05-14 11:25:30 +0200

    Update auth utils

--- hooks/auth/use-auth-utils.ts
+++ hooks/auth/use-auth-utils.ts
@@ -18,23 +18,24 @@ export function useAuthUtils() {
    * @param redirectTo URL opzionale per il redirect dopo il logout
    */
   const logout = async () => {
+    // Create logout url to logout from the OIDC provider
     const idToken = (session as any).token?.idToken;
-    console.log("idToken", idToken);
     const logoutEndpoint = `${process.env.NEXT_PUBLIC_OIDC_ISSUER}/protocol/openid-connect/logout`;
     const logoutUrl = `${logoutEndpoint}?id_token_hint=${idToken}&post_logout_redirect_uri=${process.env.NEXT_PUBLIC_NEXTAUTH_URL}`;
 
-    console.log("logoutUrl", logoutUrl);
     // Prima invalidiamo tutte le query con userData o quelle protette da auth
     queryClient.invalidateQueries({ queryKey: ["userData"] });
 
     // Rimuovi completamente i dati dalla cache per evitare dati vecchi
     queryClient.removeQueries({ queryKey: ["userData"] });
 
-    if (logoutUrl) {
+    if (idToken) {
       // Con redirect automatico di NextAuth
       await signOut({
         redirect: false,
       });
+
+      // Redirect to the logout url
       window.location.href = logoutUrl;
     } else {
       // Senza redirect automatico, lo gestiamo manualmente
