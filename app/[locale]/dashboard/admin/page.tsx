02ff2930ee01bb18369c55958ca6639f9c4d0c8e
Antonio Perchinumio <antonio_perchinumio@otb.net>
2025-06-02 12:07:27 +0200

    Change scrolling in admin console

--- app/[locale]/dashboard/admin/page.tsx
+++ app/[locale]/dashboard/admin/page.tsx
@@ -8,28 +8,34 @@ export default function AdminPage() {
   const t = useTranslations("AdminDashboard");
 
   return (
-    <div className="container mx-auto py-8 flex flex-col gap-4 px-8 overflow-auto">
-      <h1 className="text-3xl font-bold mb-8">{t("title")}</h1>
+    <div className="h-full flex flex-col">
+      <div className="w-full px-4 pt-8">
+        <h1 className="text-3xl font-bold mb-8">{t("title")}</h1>
 
-      <Tabs defaultValue="collections" className="w-full">
-        <TabsList className="grid w-full grid-cols-3">
-          <TabsTrigger value="collections">{t("tabs.collections")}</TabsTrigger>
-          <TabsTrigger value="upload">{t("tabs.upload")}</TabsTrigger>
-          <TabsTrigger value="settings">{t("tabs.settings")}</TabsTrigger>
-        </TabsList>
+        <Tabs defaultValue="collections" className="w-full ">
+          <TabsList className="grid w-full grid-cols-3 mb-4">
+            <TabsTrigger value="collections">
+              {t("tabs.collections")}
+            </TabsTrigger>
+            <TabsTrigger value="upload">{t("tabs.upload")}</TabsTrigger>
+            <TabsTrigger value="settings">{t("tabs.settings")}</TabsTrigger>
+          </TabsList>
 
-        <TabsContent value="collections">
-          <CollectionsTab />
-        </TabsContent>
+          <div className="h-[calc(100vh-204px)] overflow-auto scrollbar-hide">
+            <TabsContent value="collections" className="h-full">
+              <CollectionsTab />
+            </TabsContent>
 
-        <TabsContent value="upload">
-          <UploadTab />
-        </TabsContent>
+            <TabsContent value="upload" className="h-full">
+              <UploadTab />
+            </TabsContent>
 
-        <TabsContent value="settings">
-          <SettingsTab />
-        </TabsContent>
-      </Tabs>
+            <TabsContent value="settings" className="h-full">
+              <SettingsTab />
+            </TabsContent>
+          </div>
+        </Tabs>
+      </div>
     </div>
   );
 }
